337ba694273aaed165742cc4edd863b9cc7cb358
Luis Antonio Castro <luisantoniokoyari@gmail.com>
2024-06-24 18:39:31 +0200

    form

--- my-app/components/postForm.tsx
+++ my-app/components/postForm.tsx
@@ -17,15 +17,15 @@ export default function PostForm() {
         type="text"
         name="name"
         placeholder="author name"
-        className="border p-2 pl-3 rounded-full mr-2"
+        className="border p-2 pl-3 rounded-full mr-2 m-1"
       />
       <input
         type="text"
         name="quote"
         placeholder="quote"
-        className="border p-2 pl-3 rounded-full mr-2"
+        className="border p-2 pl-3 rounded-full mr-2 m-1"
       />
-      <button className="rounded-full text-white bg-teal-400 hover:bg-teal-500 transition duration-500 ease-in-out  p-2">
+      <button className="rounded-full text-white bg-teal-400 hover:bg-teal-500 transition duration-500 ease-in-out pl-4 pr-4 p-2 m-1">
         Add Quote
       </button>
     </form>
